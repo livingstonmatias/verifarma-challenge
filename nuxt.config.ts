00d2edbbf8cb91206e2776f27cb6a4638bb4d698
matias <livingstonmatias@gmail.com>
2025-07-23 18:56:12 -0300

    se agrega modulo eslint y configuraciones

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+	modules: ['@nuxt/eslint'],
+	devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				quotes: 'single',
+				semi: false,
+			},
+		},
+	},
 })
