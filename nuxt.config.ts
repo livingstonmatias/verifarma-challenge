47908a62c30536419f15fd332966efce47840085
matias <livingstonmatias@gmail.com>
2025-07-23 19:08:48 -0300

    se agrega modulo de test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint'],
+	modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
 	devtools: { enabled: true },
 	compatibilityDate: '2025-07-15',
 	eslint: {
