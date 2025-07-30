73eb7094c8a762e05e3fbdb921239b8f25079475
matias <livingstonmatias@gmail.com>
2025-07-29 21:18:00 -0300

    agrega configuracion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				modules: [
-								'@nuxt/eslint',
-								'@nuxt/test-utils/module',
-								'vuetify-nuxt-module',
-								'nuxt-auth-utils',
-								'nuxt-swiper',
-				],
-				devtools: { enabled: true },
-				css: ['@@/assets/scss/main.scss'],
-				compatibilityDate: '2025-07-15',
-				eslint: {
-								config: {
-												stylistic: {
-																indent: 'tab',
-																quotes: 'single',
-																semi: false,
-												},
-								},
-				},
-				vuetify: {
-								moduleOptions: {
-												/* module specific options */
-								},
-								vuetifyOptions: {
-												theme: {
-																defaultTheme: 'dark',
-												},
-								},
-				},
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/test-utils/module',
+		'vuetify-nuxt-module',
+		'nuxt-auth-utils',
+		'nuxt-swiper',
+	],
+	ssr: true,
+	devtools: { enabled: true },
+	css: ['@@/assets/scss/main.scss'],
+	compatibilityDate: '2025-07-15',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				quotes: 'single',
+				semi: false,
+			},
+		},
+	},
+	vuetify: {
+		moduleOptions: {
+			/* module specific options */
+		},
+		vuetifyOptions: {
+			theme: {
+				defaultTheme: 'dark',
+			},
+		},
+	},
 })
